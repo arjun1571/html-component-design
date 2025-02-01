fc0a26bf2bc9b9b8658d6f8370cd5fd7c0f6ef44
ARJUN CHANDRA DAS <arjundasback515@gmail.com>
2025-02-02 00:49:12 +0600

    this is first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+        fontFamily: {
+            sans: ['Arial','sans-serif',"Verdana"],
+        },
+    },
+},
+  plugins: [],
+};
